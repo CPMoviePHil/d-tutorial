206a8d24a40b2ae1e96278a971ff2ada05ca0d1e
cpmoviephil <you9215712@gmail.com>
2020-10-29 23:05:40 +0800

    views +detail

--- poll/views.py
+++ poll/views.py
@@ -1,27 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 from django.http import HttpResponse
 from .models import Questions, Answers
 # Create your views here.
-
 
 
 def index(request):
     latest_question_list = Questions.objects.order_by('-publication_date')[:5]
     return render(request, 'poll/index.html', {'latest_question_list': latest_question_list})
 
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Questions.objects.get(pk=question_id)
+    except Questions.DoesNotExist:
+        raise Http404('question is not found')
+    return render(request, "poll/detail.html", {"q": question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
+
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-# def index(request):
-#     import json
-#     from urllib import request
-#     url = 'https://www.xtube.com/webmaster/api/getcategorylist/'
-#     data = request.urlopen(url).read().decode("utf-8")
-#     return render(request, 'poll/forapi.html', {'data': data})
+
+def forNotFound():
+    pass

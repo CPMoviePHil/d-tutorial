47e642e0ccf7f4a4111218583c41f1e7a10ce3e9
cpmoviephil <you9215712@gmail.com>
2020-10-30 19:47:53 +0800

    +register model

--- register/models.py
+++ register/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Members(models.Model):
+    account = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    mail = models.CharField(max_length=200)
+
+
+class Profile(models.Model):
+    profile = models.ForeignKey(Members, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=200)
+    age = models.IntegerField(default=1)
+    birthday = models.DateTimeField()
+
